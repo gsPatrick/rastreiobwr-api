6fabc9821a5801c2b067ee473ca3de5f81521097
unknown <patrickgsiqueira@hotmail.com>
2025-07-01 05:37:59 -0300

    tirando whatsapp

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('rastreioapi', 'rastreioapi', 'rastreioapi', {
     host: 'n8n_rastreioapi',
-    dialect: 'postgres',
+    dialect: 'mariadb',
     port: 5432,
     logging: false
 });
